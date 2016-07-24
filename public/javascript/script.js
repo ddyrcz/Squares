d394c721534c4d3df303b42bf0277d0c520ba5a4
Dawid Dyrcz <dyrcu15@gmail.com>
2016-07-24 02:01:47 +0200

    Fully support for multiplayer experience

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -2,30 +2,62 @@ const LEFT_KEY_CODE = 37;
 const RIGHT_KEY_CODE = 39;
 const UP_KEY_CODE = 38;
 const DOWN_KEY_CODE = 40;
+var shift = 5;
+
+var socket = io.connect();
+var square;
 
 $(document).ready(() => {
+
+    socket.on('square', (currentSquare) => {
+        square = currentSquare;
+        console.log(square);
+    });
+
+    socket.on('full', (err) => {
+        console.log(err);
+    });
+
+    socket.on('right', (square) => {
+        var id = '#' + square;
+        var position = parseInt($(id).css('right')) - shift;                
+        $(id).css('right', position + 'px');           
+    });
+
+    socket.on('left', (square) => {
+        var id = '#' + square;
+        var position = parseInt($(id).css('right')) + shift;                
+        $(id).css('right', position + 'px');           
+    });
+
+    socket.on('up', (square) => {
+        var id = '#' + square;
+        var position = parseInt($(id).css('top')) - shift;                
+        $(id).css('top', position + 'px');           
+    });
+
+    socket.on('down', (square) => {
+        var id = '#' + square;
+        var position = parseInt($(id).css('top')) + shift;                
+        $(id).css('top', position + 'px');           
+    });
 
     $(document).keydown((event) => {
 
         var keyCode = event.keyCode;
-        var shift = 5;
-        
+
         switch (keyCode) {
             case LEFT_KEY_CODE:
-                var rightPosition = parseInt($('#green').css('right')) + shift;                
-                $('#green').css('right', rightPosition + 'px');                                
+                socket.emit('left', square);                                
                 break;
-            case RIGHT_KEY_CODE:                   
-                var rightPosition = parseInt($('#green').css('right')) - shift;                
-                $('#green').css('right', rightPosition + 'px');             
+            case RIGHT_KEY_CODE:                                   
+                socket.emit('right', square);
                 break;
             case UP_KEY_CODE:                
-                var rightPosition = parseInt($('#green').css('top')) - shift;                
-                $('#green').css('top', rightPosition + 'px');        
+                socket.emit('up', square);        
                 break;
             case DOWN_KEY_CODE:                
-                var rightPosition = parseInt($('#green').css('top')) + shift;                
-                $('#green').css('top', rightPosition + 'px');        
+                socket.emit('down', square);        
                 break;
         }        
     });
--- server.js
+++ server.js
@@ -9,7 +9,45 @@ var server = http.createServer((req, res) => {
 }).listen(8000);
 
 var io = socketio.listen(server);
+var squares = {};
+var playerNumber = 0;
+var assignSquare = {};
 
 io.sockets.on('connection', (socket) => {
     console.log('connection accepted');
+
+    playerNumber += 1;
+
+    socket.on('right', (square) => {
+        io.sockets.emit('right', square);
+    });
+
+    socket.on('left', (square) => {
+        io.sockets.emit('left', square);
+    });
+
+    socket.on('down', (square) => {
+        io.sockets.emit('down', square);
+    });
+
+    socket.on('up', (square) => {
+        io.sockets.emit('up', square);
+    });
+
+    if(playerNumber > 2){
+        socket.emit('full', 'The server is full');
+        return;
+    }    
+
+    if(squares.green == undefined){
+        squares.green = socket.id;  
+        assignSquare[socket.id] = 'green';          
+    }else{
+        squares.red = socket.id;
+        assignSquare[socket.id] = 'red';
+    }
+
+    var square = assignSquare[socket.id];
+    
+    socket.emit('square',square);
 });
