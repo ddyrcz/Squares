c61bd759446f0cbb6cd87d895371120098a04e51
Dawid Dyrcz <dyrcu15@gmail.com>
2016-07-24 02:38:37 +0200

    Limit square movement to the board range

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -20,26 +20,29 @@ $(document).ready(() => {
 
     socket.on('right', (square) => {
         var id = '#' + square;
-        var position = parseInt($(id).css('right')) - shift;                
-        $(id).css('right', position + 'px');           
+        var position = parseInt($(id).css('right')) - shift;
+        if (position >= 0)
+            $(id).css('right', position + 'px');
     });
 
     socket.on('left', (square) => {
         var id = '#' + square;
-        var position = parseInt($(id).css('right')) + shift;                
-        $(id).css('right', position + 'px');           
+        var position = parseInt($(id).css('right')) + shift;        
+        if (parseInt($('#board').css('width')) >= position + parseInt($(id).css('width')))
+            $(id).css('right', position + 'px');
     });
-
     socket.on('up', (square) => {
         var id = '#' + square;
-        var position = parseInt($(id).css('top')) - shift;                
-        $(id).css('top', position + 'px');           
+        var position = parseInt($(id).css('top')) - shift;
+        if (position >= 0)
+            $(id).css('top', position + 'px');
     });
 
     socket.on('down', (square) => {
         var id = '#' + square;
-        var position = parseInt($(id).css('top')) + shift;                
-        $(id).css('top', position + 'px');           
+        var position = parseInt($(id).css('top')) + shift;        
+        if (parseInt($('#board').css('height')) >= position + parseInt($(id).css('height')))
+            $(id).css('top', position + 'px');
     });
 
     $(document).keydown((event) => {
@@ -48,18 +51,18 @@ $(document).ready(() => {
 
         switch (keyCode) {
             case LEFT_KEY_CODE:
-                socket.emit('left', square);                                
+                socket.emit('left', square);
                 break;
-            case RIGHT_KEY_CODE:                                   
+            case RIGHT_KEY_CODE:
                 socket.emit('right', square);
                 break;
-            case UP_KEY_CODE:                
-                socket.emit('up', square);        
+            case UP_KEY_CODE:
+                socket.emit('up', square);
                 break;
-            case DOWN_KEY_CODE:                
-                socket.emit('down', square);        
+            case DOWN_KEY_CODE:
+                socket.emit('down', square);
                 break;
-        }        
+        }
     });
 
 });
