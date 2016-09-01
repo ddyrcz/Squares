c2cba38b87d0eafc9d5a1b2d893899173f6496fd
Dawid Dyrcz <dyrcu15@gmail.com>
2016-09-01 18:17:09 +0200

    Remove commented code

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -21,31 +21,27 @@ $(document).ready(() => {
     socket.on('right', (data) => {
         var id = '#' + data.square;
         var position = data.newPosition + 'px';
-        //if (newPosition >= 0)
-            $(id).css('right', position);
+        $(id).css('right', position);
     });
 
     socket.on('left', (data) => {
         var id = '#' + data.square;
-        var position = data.newPosition;               
-        //if (parseInt($('#board').css('width')) >= newPosition + parseInt($(id).css('width')))
-            $(id).css('right', position);
+        var position = data.newPosition;
+        $(id).css('right', position);
     });
     socket.on('up', (data) => {
         var id = '#' + data.square;
         var position = data.newPosition + 'px';
-        //if (newPosition >= 0)
-            $(id).css('top', position);
+        $(id).css('top', position);
     });
 
     socket.on('down', (data) => {
         var id = '#' + data.square;
-        var position = data.newPosition + 'px';   
-        //if (parseInt($('#board').css('height')) >= newPosition + parseInt($(id).css('height')))
-            $(id).css('top', position);
+        var position = data.newPosition + 'px';
+        $(id).css('top', position);
     });
 
-    socket.emit('board', ({'width':$('#board').css('width'), 'height':$('#board').css('height')}))
+    socket.emit('board', ({ 'width': $('#board').css('width'), 'height': $('#board').css('height') }))
 
     socket.on('init', (data) => {
         $('#red').css('top', data.red.top + 'px');
